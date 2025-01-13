1a54ebce21f680e0f847b2815da29ad1ee3c893c
Kyle Okwach <102046593+KyleOkwach@users.noreply.github.com>
2025-01-13 21:18:14 +0300

    Time: 3 ms (66.19%), Space: 19.1 MB (34.62%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -7,10 +7,19 @@ class Solution:
             if t not in operators:
                 num_buffer.append(t)
             else:
-                res = eval(f"{num_buffer[-2]}{t}{num_buffer[-1]}")
+                res = self.evalStack(int(num_buffer[-2]), int(num_buffer[-1]), t)
                 num_buffer.pop()
                 num_buffer.pop()
                 num_buffer.append(int(res))
         
         return int(num_buffer[0])
-        
+    
+    def evalStack(self, a, b, op) -> int:
+        if op == "+":
+            return a + b
+        elif op == "-":
+            return a - b
+        elif op == "*":
+            return a * b
+        elif op == "/":
+            return a / b
