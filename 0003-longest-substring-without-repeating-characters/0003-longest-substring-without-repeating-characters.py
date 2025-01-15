8560c55bf4abe3ed9cdf23b52de0e4c22ad4e2f3
Kyle Okwach <102046593+KyleOkwach@users.noreply.github.com>
2025-01-15 23:22:02 +0300

    Time: 23 ms (32.6%), Space: 17.9 MB (27.26%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -8,12 +8,10 @@ class Solution:
         
         for r in range(len(s)):
             if s[r] in res:
-                print("Clearing and shifting...")
                 while s[r] in res:
                     res.remove(s[l])
                     l += 1
             res.add(s[r])
-            print(f"left[{l}]: '{s[l]}'\tright[{r}]: '{s[r]}'\t|\tres: '{res}'")
             
             longest = max(longest, len(res))
         
